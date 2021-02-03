7b9461ca2f4e59850686a9e3c271d877d85fe8a3
eurowondollaryen <sehoakasayho@naver.com>
2021-02-03 22:07:29 +0900

    login form

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -8,7 +8,7 @@ class Header extends React.Component {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <Link to="/" className="navbar-brand">
-              React
+              설문
             </Link>
             <button
               className="navbar-toggler"
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,43 @@ import React from 'react';
 
 class Home extends React.Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div className="container">
+        <div className="wrap-login100">
+          <form className="login100-form validate-form">
+            <span className="login100-form-title p-b-26">로그인</span>
+            <span className="login100-form-title p-b-48">
+              <i className="zmdi zmdi-font" />
+            </span>
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Valid email is: a@b.c"
+            >
+              <input className="input100" type="text" name="email" />
+              <span className="focus-input100" data-placeholder="Email" />
+            </div>
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Enter password"
+            >
+              <span className="btn-show-pass">
+                <i className="zmdi zmdi-eye" />
+              </span>
+              <input className="input100" type="password" name="pass" />
+              <span className="focus-input100" data-placeholder="Password" />
+            </div>
+            <div className="container-login100-form-btn">
+              <div className="wrap-login100-form-btn">
+                <div className="login100-form-bgbtn" />
+                <button type="submit" className="login100-form-btn">
+                  로그인
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
