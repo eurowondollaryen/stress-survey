5fa74a3790bbd9e70275239c936f44902ca64e71
eurowondollaryen <sehoakasayho@naver.com>
2021-02-04 23:58:57 +0900

    added auth logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/layout/header.jsx';
 import Content from './components/layout/content.jsx';
+import { signIn } from './logics/auth.js';
 
 class App extends React.Component {
   render() {
--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -10,39 +10,6 @@ class Header extends React.Component {
             <Link to="/" className="navbar-brand">
               설문
             </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link to="/home" className="nav-link">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/about" className="nav-link">
-                    About
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/contact" className="nav-link">
-                    Contact
-                  </Link>
-                </li>
-              </ul>
-            </div>
           </div>
         </nav>
       </>
