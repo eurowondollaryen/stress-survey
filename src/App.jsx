abeeedfbe547a3606090237c9078aba31f153298
eurowondollaryen <sehoakasayho@naver.com>
2021-02-05 23:56:00 +0900

    authentication class added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './components/layout/header.jsx';
-import Content from './components/layout/content.jsx';
+import Authentication from './components/layout/authentication.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <Content />
+        <Authentication />
       </div>
     );
   }
