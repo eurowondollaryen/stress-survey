b95a0e9379f3dbe6ff7223a6c9531453034fd8ae
eurowondollaryen <sehoakasayho@naver.com>
2021-02-09 23:28:39 +0900

    auth logic added to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Authentication from './components/layout/authentication.jsx';
 
 class App extends React.Component {
   render() {
+    const [user, setUser] = useState(null);
+    const authenticated = user != null;
+
+    const login = ({ email, password }) => setUser(signIn({ email, password }));
+    const logout = () => setUser(null);
     return (
       <div>
         <Header />
