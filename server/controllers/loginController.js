4413b2e7d9ef106752d50d113a3de256f15a9272
eurowondollaryen <sehoakasayho@naver.com>
2021-02-13 00:36:05 +0900

    login backend end

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,7 +1,18 @@
 const login = require("../models/login.js");
 
-const dologin = (req, res) => {
-  res.json(login.login([]));
+const dologin = async (req, res) => {
+  const { id, pw } = req.body;
+  try {
+    const loginResult = await login.login([id, pw]);
+    console.log("[loginController][dologin] id : " + id + ", pw : " + pw);
+    if (loginResult.length != 1) {
+      res.json({ message: "fail" });
+    } else {
+      res.json(loginResult[0]);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 exports.dologin = dologin;
--- server/models/login.js
+++ server/models/login.js
@@ -1,7 +1,11 @@
 const pool = require("../db.js").pool;
 
 exports.login = async (parameters) => {
-  const result = await pool.query(`SELECT * FROM ICTSURVEYUSER`);
+  const result = await pool.query(
+    `SELECT USER_ID, USER_NAME, USER_EMAIL, COMP_NAME, DEPT_NAME, USER_DIV, INST_TIME
+     FROM ICTSURVEYUSER WHERE USER_ID = $1 AND USER_PW = $2`,
+    parameters
+  );
 
   return result.rows;
 };
