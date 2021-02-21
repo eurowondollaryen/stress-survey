8350ceee397091165053f828c9fce7da472effe9
eurowondollaryen <sehoakasayho@naver.com>
2021-02-21 23:05:48 +0900

    todo : online

--- server/router.js
+++ server/router.js
@@ -2,8 +2,8 @@ const loginController = require("./controllers/loginController.js");
 
 exports.route = (app) => {
   //login
-  app.post("/login", loginController.dologin);
-
+  app.post("/login", loginController.doLogin);
+  app.post("/logincheck", loginController.checkLogin);
   app.get("*", (req, res) => {
     res.end("<head><title>404</title></head><body><h1>404 error</h1></body>");
   });
