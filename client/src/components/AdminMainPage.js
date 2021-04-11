76681e1e4930219509ddb31dd5af2b6c98ba8969
eurowondollaryen <sehoakasayho@naver.com>
2021-04-11 23:32:18 +0900

    react to ejs

--- client/src/components/AdminMainPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const AdminMainPage = ({ user }) => {
-  const { id, name, email } = user || {};
-  return <div className="container">hello {name}</div>;
-};
-
-export default AdminMainPage;
--- client/src/components/NotFoundPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFoundPage = () => {
-  return <div className="container">404 not found</div>;
-};
-
-export default NotFoundPage;
