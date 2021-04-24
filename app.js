385815248e4e64451e9da77d3604a3044332a051
eurowondollaryen <sehoakasayho@naver.com>
2021-04-24 23:52:50 +0900

    https

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ const session = require("express-session");
 //body-parser : FOR HANDLING POST DATA
 const bodyParser = require("body-parser");
 
+//**https
+//https://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
+//https://eunsukimme.github.io/nodejs/2019/09/20/Express-SSL-HTTPS/
+const https = require("https");
+/*
+TODO: GET SSL
+const options = {
+  key: fs.readFileSync(__dirname + '/인증서경로/domain_xxxxx.key.pem')
+  cert: fs.readFileSync(__dirname + '/인증서경로/domain_xxxxx.crt.pem')
+  ca: fs.readFileSync(__dirname + '/인증서경로/ca-chain-bundle.pem')
+};
+const options = {
+  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+};
+const options = {
+  pfx: fs.readFileSync('test/fixtures/test_cert.pfx'),
+  passphrase: 'sample'
+};
+*/
+
 //middleware
 app.use(cors());
 app.use(express.json());
