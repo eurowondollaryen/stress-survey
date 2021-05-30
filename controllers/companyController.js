9e4d0b6f583427b1b2ec84b53b010d564726abcb
eurowondollaryen <sehoakasayho@naver.com>
2021-05-30 23:59:34 +0900

    added companycontroller

---
+++ controllers/companyController.js
@@ -0,0 +1,74 @@
+const user = require("../models/company.js");
+
+//post : req.body, get(url) : req.query
+const searchCompany = async (req, res) => {
+  const {} = req.query;
+  try {
+    const result = await user.searchUser([]);
+    console.log("[userController][searchUser] request success!");
+    var responseObj = result;
+    //console.log(responseObj);
+    res.json(responseObj);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const addCompany = async (req, res) => {
+  //USER_ID, USER_PW, USER_NAME, USER_EMAIL, COMP_NAME, DEPT_NAME, USER_DIV
+  const {
+    USER_ID,
+    USER_PW,
+    USER_NAME,
+    USER_EMAIL,
+    COMP_NAME,
+    DEPT_NAME,
+    USER_DIV,
+  } = req.body;
+  try {
+    console.log(
+      USER_ID,
+      USER_PW,
+      USER_NAME,
+      USER_EMAIL,
+      COMP_NAME,
+      DEPT_NAME,
+      USER_DIV
+    );
+    const result = await user.addUser([
+      USER_ID,
+      USER_PW,
+      USER_NAME,
+      USER_EMAIL,
+      COMP_NAME,
+      DEPT_NAME,
+      USER_DIV,
+    ]);
+    console.log("[userController][addUser] request success!");
+    //console.log(result);
+    res.json(result);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  //user_list[]가 1개만 담고 있으면, string으로 가져와서, 분기 처리
+  let user_list;
+  if (typeof req.body["user_list[]"] === "string")
+    user_list = [req.body["user_list[]"]];
+  else user_list = req.body["user_list[]"];
+
+  try {
+    console.log("[userController][deleteUser] request success!");
+    await user.deleteUser(user_list);
+    console.log(user_list);
+    res.json({ message: "ok" });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+exports.serachUser = searchUser;
+exports.addUser = addUser;
+exports.deleteUser = deleteUser;
