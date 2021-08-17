dd9654df20713718fb1e089aa09c1043e0fcc4f6
eurowondollaryen <sehoakasayho@naver.com>
2021-08-18 00:00:40 +0900

    prettier

--- controllers/calculationController.js
+++ controllers/calculationController.js
@@ -5,9 +5,16 @@ const calculateSurveyResult = async (req, res) => {
   const { USER_ID, SRVY_ID, START_TIME, END_TIME } = req.body;
   try {
     //console.log(USER_ID, COMPANY_NAME);
-    const result = await calculation.getCalculateData([USER_ID, SRVY_ID, START_TIME, END_TIME]);
+    const result = await calculation.getCalculateData([
+      USER_ID,
+      SRVY_ID,
+      START_TIME,
+      END_TIME,
+    ]);
     //todo: calculate and save result with result
-    console.log("[calculationController][calculateSurveyResult] request success!");
+    console.log(
+      "[calculationController][calculateSurveyResult] request success!"
+    );
     var responseObj = result;
     console.log(responseObj);
     res.json(responseObj);
